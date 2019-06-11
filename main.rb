178d869e95a9bfc3a6f9de2d526035a705465679
vsh1ft <kastytis.ch@gmail.com>
2019-06-11 12:50:03 +0300

    BA-794: base path code simplified

--- main.rb
+++ main.rb
@@ -1,11 +1,3 @@
-require './version_changer'
-require_relative './csv_converter/csv_converter'
-
-
-if ARGV[3] == nil
-  Dir.chdir('..') do
-    ARGV[3] = File.expand_path(File.dirname(__FILE__))
-  end
-end
+require_relative './version_changer'
 
 VersionChanger.new.change(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
--- version_changer.rb
+++ version_changer.rb
@@ -1,6 +1,7 @@
 class VersionChanger
 
   def change(config_path, old_version, new_version, base_path)
+    base_path ||= '../'
     locations = CsvConverter.new.convert(config_path)
     for loc in locations do
       replace_in_file(loc, old_version, new_version, base_path)
@@ -23,7 +24,6 @@ class VersionChanger
     File.open(base_path + '/' + location.relative_path, "w") do |f|
       f.write(file_with_changed_versions)
     end
-
   end
 
 end
