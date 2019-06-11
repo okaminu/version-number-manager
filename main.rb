1070e4067154856ca7a5c53ba30bc29110ed7f00
vsh1ft <kastytis.ch@gmail.com>
2019-06-11 12:27:18 +0300

    BA-794: retrieves project directory path

--- main.rb
+++ main.rb
@@ -1,4 +1,11 @@
 require './version_changer'
 require_relative './csv_converter/csv_converter'
 
+
+if ARGV[3] == nil
+  Dir.chdir('..') do
+    ARGV[3] = File.expand_path(File.dirname(__FILE__))
+  end
+end
+
 VersionChanger.new.change(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
--- version_changer.rb
+++ version_changer.rb
@@ -1,16 +1,16 @@
 class VersionChanger
 
-  def change(config_path, base_path, old_version, new_version)
+  def change(config_path, old_version, new_version, base_path)
     locations = CsvConverter.new.convert(config_path)
     for loc in locations do
-      replace_in_file(loc, base_path, old_version, new_version)
+      replace_in_file(loc, old_version, new_version, base_path)
     end
   end
 
   private
 
-  def replace_in_file(location, base_path, old_version, new_version)
-    file = File.read(base_path + location.relative_path)
+  def replace_in_file(location, old_version, new_version, base_path)
+    file = File.read(base_path + '/' + location.relative_path)
     actual_count = file.scan(old_version).count
 
     unless actual_count == location.occurances
@@ -20,7 +20,7 @@ class VersionChanger
 
     file_with_changed_versions = file.gsub(old_version, new_version)
 
-    File.open(base_path + location.relative_path, "w") do |f|
+    File.open(base_path + '/' + location.relative_path, "w") do |f|
       f.write(file_with_changed_versions)
     end
 
