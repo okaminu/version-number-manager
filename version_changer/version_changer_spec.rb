7236a4e2a4394e270940d78d46447ce25ed42663
Andrius Degutis <andriusdgt@gmail.com>
2019-06-11 15:30:02 +0300

    BA-794: removed unnecessary test

--- version_changer/version_changer_spec.rb
+++ version_changer/version_changer_spec.rb
@@ -2,7 +2,7 @@ require_relative 'version_changer'
 
 describe VersionChanger do
 
-  it 'writes read file content with substituted version numbers' do
+  it 'writes read file content with known number of substituted version numbers' do
     writable_file_spy = instance_double("FileToWrite")
     allow(File).to receive(:read).with("/file/path").and_return "file_content \n version:1.0.0 \n dependencyA:1.0.0"
     allow(File).to receive(:open).with("/file/path", "w").and_yield writable_file_spy
@@ -13,16 +13,6 @@ describe VersionChanger do
   end
 
   it 'ignores unmatched versions' do
-    writable_file_spy = instance_double("FileToWrite")
-    allow(File).to receive(:read).with("/file/path").and_return "file_content \n version:1.0.0 \n dependencyA:1.0.0 \n dependencyB:1.2.0"
-    allow(File).to receive(:open).with("/file/path", "w").and_yield writable_file_spy
-
-    expect(writable_file_spy).to receive(:write).with "file_content \n version:2.0.0 \n dependencyA:2.0.0 \n dependencyB:1.2.0"
-
-    VersionChanger.new.substitute("/file/path", "1.0.0", "2.0.0", 2)
-  end
-
-  it 'substitutes only a known number of version numbers' do
     writable_file_spy = instance_double("FileToWrite")
     allow(File).to receive(:read).with("/file/path").and_return "file_content \n version:1.0.0 \n dependencyA:1.0.0 \n dependencyB:1.2.0"
     allow(File).to receive(:open).with("/file/path", "w").and_yield writable_file_spy
