26245bf79d9cc6c6746159d65d04eefd857483fb
Andrius Degutis <andriusdgt@gmail.com>
2019-06-13 15:43:47 +0300

    BA-794: naming improvements

--- integration_tests_spec.rb
+++ integration_tests_spec.rb
@@ -2,7 +2,7 @@ describe 'Integration test' do
   let(:content)         { "file_content \n version:1.2.5 \n dependencyA:1.2.5 \n dependencyB:1.2.0" }
   let(:updated_content) { "file_content \n version:1.3.1 \n dependencyA:1.3.1 \n dependencyB:1.2.0" }
 
-  context 'By providing all the necessary arguments' do
+  context 'by providing all the necessary arguments' do
     before(:each) { create_temp_files('/tmp') }
     after(:each) { delete_temp_files('/tmp') }
 
@@ -13,19 +13,19 @@ describe 'Integration test' do
     end
   end
 
-  context 'By omitting base path argument' do
+  context 'by omitting base path argument' do
     before(:each) { create_temp_files(Dir.pwd, "#{File.basename(Dir.getwd)}/") }
     after(:each) { delete_temp_files(Dir.pwd) }
 
-    it 'replaces version numbers in desired file located in relative directory' do
+    it 'replaces version numbers in desired file, searched from parent directory' do
       system "ruby main.rb #{Dir.pwd}/config.csv 1.2.5 1.3.1"
 
       expect(File.read("#{Dir.pwd}/test.example")).to eql(updated_content)
     end
   end
 
-  def create_temp_files(directory, relative_path_prefix = '')
-    File.open("#{directory}/config.csv", 'w')   { |f| f.write("#{relative_path_prefix}test.example,2") }
+  def create_temp_files(directory, subject_subdirectory = '')
+    File.open("#{directory}/config.csv", 'w')   { |f| f.write("#{subject_subdirectory}test.example,2") }
     File.open("#{directory}/test.example", 'w') { |f| f.write(content) }
   end
 
