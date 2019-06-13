b6601e167480634f9d8c172236269ce4ce8527c5
Andrius Degutis <andriusdgt@gmail.com>
2019-06-13 14:41:02 +0300

    BA-794: naming fixes

--- integration_tests_spec.rb
+++ integration_tests_spec.rb
@@ -1,22 +1,20 @@
-require 'tempfile'
-
 describe 'Integration test' do
   let(:content)         { "file_content \n version:1.2.5 \n dependencyA:1.2.5 \n dependencyB:1.2.0" }
   let(:updated_content) { "file_content \n version:1.3.1 \n dependencyA:1.3.1 \n dependencyB:1.2.0" }
 
   before(:each) do
-    File.open('/tmp/test.csv', 'w')  { |f| f.write('test.test,2') }
-    File.open('/tmp/test.test', 'w') { |f| f.write(content) }
+    File.open('/tmp/config.csv', 'w')   { |f| f.write('test.example,2') }
+    File.open('/tmp/test.example', 'w') { |f| f.write(content) }
   end
 
   after(:each) do
-    File.delete('/tmp/test.csv')
-    File.delete('/tmp/test.test')
+    File.delete('/tmp/config.csv')
+    File.delete('/tmp/test.example')
   end
 
   it 'replaces version numbers in desired file' do
-    system 'ruby main.rb /tmp/test.csv 1.2.5 1.3.1 /tmp/'
+    system 'ruby main.rb /tmp/config.csv 1.2.5 1.3.1 /tmp/'
 
-    expect(File.read('/tmp/test.test')).to eql(updated_content)
+    expect(File.read('/tmp/test.example')).to eql(updated_content)
   end
 end
