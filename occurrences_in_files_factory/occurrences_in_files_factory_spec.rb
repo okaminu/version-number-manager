681336488feb62306c42235ba6c08ca45804bcf4
vsh1ft <kastytis.ch@gmail.com>
2019-06-14 09:30:55 +0300

    BA-794: fixed naming in a test

--- occurrences_in_files_factory/occurrences_in_files_factory_spec.rb
+++ occurrences_in_files_factory/occurrences_in_files_factory_spec.rb
@@ -4,13 +4,13 @@ require_relative 'occurrences_in_files_factory'
 describe OccurrencesInFilesFactory do
   it 'converts csv file to occurrences in files' do
     filename = 'data.csv'
-    csv_row = ['path', 5]
-    allow(CSV).to receive(:foreach).with(filename).and_yield csv_row
+    expected_occurrences = ['path', 5]
+    allow(CSV).to receive(:foreach).with(filename).and_yield expected_occurrences
 
-    occurrences = OccurrencesInFilesFactory.from_csv(filename)
+    actual_occurrences = OccurrencesInFilesFactory.from_csv(filename)
 
-    expect(csv_row[0]).to eql(occurrences[0].relative_path)
-    expect(csv_row[1]).to eql(occurrences[0].occurrences)
-    expect(occurrences.length).to eql(1)
+    expect(expected_occurrences[0]).to eql(actual_occurrences[0].relative_path)
+    expect(expected_occurrences[1]).to eql(actual_occurrences[0].occurrences)
+    expect(actual_occurrences.length).to eql(1)
   end
 end
